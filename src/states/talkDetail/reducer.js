406f232717d5395ac7dc6da7facbc6a9d9356db1
rezafikkri <fikkri.reza@gmail.com>
2024-02-23 05:00:55 +0700

    feat: create action and reducer for talk detail

--- src/states/talkDetail/reducer.js
+++ src/states/talkDetail/reducer.js
@@ -1,3 +1,29 @@
 /**
  * @TODO: Define reducer for the talkDetail state
  */
+
+import { ActionType } from "./action";
+
+function talkDetailReducer(talkDetail = null, action = {}) {
+  switch (action.type) {
+    case ActionType.RECEIVE_TALK_DETAIL:
+      return action.payload.talkDetail;
+
+    // Penasaran kenapa harus clear talk detail
+    case ActionType.CLEAR_TALK_DETAIL:
+      return null;
+
+    case ActionType.TOGGLE_LIKE_TALK_DETAIL:
+      return {
+        ...talkDetail,
+        likes: talkDetail.likes.includes(action.payload.userId)
+          ? talkDetail.likes.filter((userId) => userId !== action.payload.userId)
+          : talkDetail.likes.concat([action.payload.userId])
+      };
+
+    default:
+      return talkDetail;
+  }
+}
+
+export default talkDetailReducer;
