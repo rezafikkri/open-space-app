463a26817fb0ae7a670ca105a0941fe6ed196858
rezafikkri <fikkri.reza@gmail.com>
2024-03-21 07:21:33 +0700

    test: Menguji component react dengan react testing libray

---
+++ src/components/LoginInput.test.jsx
@@ -0,0 +1,72 @@
+import {
+  describe,
+  test,
+  expect,
+  afterEach,
+  vi,
+} from 'vitest';
+import { render, screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import matchers from '@testing-library/jest-dom/matchers';
+import LoginInput from './LoginInput';
+
+expect.extend(matchers);
+
+/**
+ * skenario testing
+ *
+ * - LoginInput component
+ *   - should handle username typing correctly
+ *   - should handle password typing correctly
+ *   - should call login function when login button is clicked
+ */
+
+describe('LoginInput component', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  test('should handle username typing correctly', async () => {
+    // Arrange
+    render(<LoginInput login={() => {}} />);
+    const usernameInput = await screen.getByPlaceholderText('Username');
+
+    // Action
+    await userEvent.type(usernameInput, 'usernametest');
+
+    // Assert
+    expect(usernameInput).toHaveValue('usernametest');
+  });
+
+  test('should handle password typing correctly', async () => {
+    // Arrange
+    render(<LoginInput login={() => {}} />);
+    const passwordInput = await screen.getByPlaceholderText('Password');
+
+    // Action
+    await userEvent.type(passwordInput, 'passwordtest');
+
+    // Assert
+    expect(passwordInput).toHaveValue('passwordtest');
+  });
+
+  test('should call login function when login button is clicked', async () => {
+    // Arrange
+    const mockLogin = vi.fn();
+    render(<LoginInput login={mockLogin} />);
+    const usernameInput = await screen.getByPlaceholderText('Username');
+    await userEvent.type(usernameInput, 'usernametest');
+    const passwordInput = await screen.getByPlaceholderText('Password');
+    await userEvent.type(passwordInput, 'passwordtest');
+    const loginButton = await screen.getByRole('button', { name: 'Login' });
+
+    // Action
+    await userEvent.click(loginButton);
+
+    // Assert
+    expect(mockLogin).toBeCalledWith({
+      id: 'usernametest',
+      password: 'passwordtest',
+    });
+  });
+});
